502ee3032493e324b37552e55e3960dab8bf1360
JhonyOwner <jvgbrisola@gmail.com>
2026-02-20 06:12:27 -0300

    Arquivo dist minificado

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-function testeGulp(txt) {
-    console.log('Teste do Gulp');
-    txt();
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCss = require('gulp-clean-css');
+
+function styles() {
+    return gulp.src('./src/styles/**/*.scss')
+        .pipe(sass())  // Compila sem compressão
+        .pipe(cleanCss())  // Minifica
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = testeGulp;
+exports.default = styles;
